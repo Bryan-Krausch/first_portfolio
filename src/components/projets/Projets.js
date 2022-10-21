a9b27bb3d6a7a7a75aac19ba8a2b88ed11e2b3a6
Bryan Krausch <contact.krausch@gmail.com>
2022-10-21 18:21:32 +0200

    Added a projects

--- src/components/projets/Projets.js
+++ src/components/projets/Projets.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 export default function Projets(){
     const [projectState, setProjectState] = useState(1)
     function increaseProjectState(){
-        if(projectState === 3){
+        if(projectState === 4){
             return
         }else{
             setProjectState(projectState + 1)
@@ -24,13 +24,17 @@ export default function Projets(){
         <React.Fragment>
             <section className="h-screen w-full flex  justify-between items-center relative overflow-x-auto">
 
-                <div className="text-2xl font-bold text-white cursor-pointer" onClick={() => {decreaseProjectState()}}>&#8592;</div>
+                {projectState !== 1 &&
+                    <div className="text-2xl font-bold text-white cursor-pointer" onClick={() => {decreaseProjectState()}}>&#8592;</div>
+                }
 
                 <div className="w-[85%] h-[50%] flex relative">
                     <ProjetsItems projectState={projectState} />
                 </div>
 
-                <div className="text-2xl font-bold text-white cursor-pointer" onClick={() => {increaseProjectState()}}>&rarr;</div>
+                {projectState !== 4 && 
+                    <div className="text-2xl font-bold text-white cursor-pointer" onClick={() => {increaseProjectState()}}>&rarr;</div>
+                }
 
             </section>
         </React.Fragment>
--- src/components/projets/ProjetsItems.js
+++ src/components/projets/ProjetsItems.js
@@ -1,6 +1,7 @@
 import spotifyProject from "../../img/projet/spotifyProject.png"
 import cryptoMonnaieProject from "../../img/projet/cryptoProject.png"
 import irealMindProject from "../../img/projet/irealMindProject.png"
+import optineoProject from "../../img/projet/optineo.png"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -107,6 +108,44 @@ export default function ProjetsItems({projectState}){
             </div>
 
         </div>
+        <div className={`${projectState !== 4 ? "hidden" : 'block'} flex`}  id="1">
+
+                <a href="https://optineo.info/">
+
+                    <div className="w-[70%] 2xl:h-full h-full relative group z-10 flex items-center">
+                        <div className="relative group">
+                            <div className={`w-full h-full bg-mediumPurple absolute top-0 l-0 opacity-40 group-hover:opacity-0 z-[50]`}></div>
+                            <img src={optineoProject} alt="spotify clone illustration" className="w-full h-full rounded z-[10]  object-cover block shadow"/>
+                        </div>
+                    </div>
+                </a>
+                
+                <div className="w-[50%] h-[70%] absolute right-0 z-20 flex items-end">
+                    <div className="w-full h-[70%] flex flex-col items-end text-white space-y-6">
+                        <h3 className="text-lightPurple text-lg font-semibold tracking-wide">Optineo</h3>
+                        <div className="bg-mediumPurple w-full lg:h-[60%] xl:h-[50%] text-right text-ligthGreyText flex items-center rounded shadow-xl px-6 py-5 ">
+                            <p className="text-xs laptop:text-xs xl:text-sm">
+                                J'ai créer ce site pour un client que j'ai eu pendant que je travaillais en <strong className="text-lightPurple">freelance</strong>.Je devais créer un 
+                                <strong className="text-lightPurple"> site vitrine</strong> pour une entreprise dans le domaine des énergies renouvellable.
+                                Le site devait contenir un <strong className="text-lightPurple">formulaire de contact</strong>.
+                            </p>
+                        </div>
+                        <ul className="flex space-x-4 text-greyText xl:text-base text-sm">
+                            <li>React</li>
+                            <li>MySQL</li>
+                            <li>Tailwind</li>
+                            <li>Hébergement OVH</li>
+                            <li>Google Analytics</li>
+                            <li>Tracking</li>
+                        </ul>
+                        <div className="mt-2 space-x-4 text-lg">
+                            <a href="https://github.com/Bryan-Krausch/Pac"><FontAwesomeIcon icon={faGithub} /></a>
+                            <a href="https://optineo.info/"><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         
     </React.Fragment>
     )
